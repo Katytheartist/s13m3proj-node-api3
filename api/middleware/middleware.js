c2a0f2d7b3308397547a6d668d5d8cda634f1e21
Katy Kane <katytheartist@gmail.com>
2024-05-01 17:33:29 -0500

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,34 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+const method = req.method
+const url = req.originalUrl
+const timestamp = new Date().toLocaleString()
+console.log(`${method} to ${url} at [${timestamp}]`)
+next()
+// DO YOUR MAGIC
 }
 
 function validateUserId(req, res, next) {
+console.log('validateUserId middleware')
+next()
   // DO YOUR MAGIC
 }
 
 function validateUser(req, res, next) {
+console.log('validateUser middleware')
+next()
   // DO YOUR MAGIC
 }
 
 function validatePost(req, res, next) {
+console.log('validatePost middleware')
+next()
   // DO YOUR MAGIC
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
