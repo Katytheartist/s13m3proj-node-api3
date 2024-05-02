f94cf985526bb6ca2798dd2f5ffe23d4bf48a113
Katy Kane <katytheartist@gmail.com>
2024-05-02 13:19:55 -0500

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -70,10 +70,17 @@ router.delete('/:id', validateUserId, async (req, res, next) => {
 
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
   console.log(req.user)
+
+  try{
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  } catch (err){
+    next(err)
+  }
 });
 
 router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
@@ -82,6 +89,8 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // and another middleware to check that the request body is valid
   console.log(req.user)
   console.log(req.text)
+
+
 });
 
 router.use((err, req, res, next)=>{
