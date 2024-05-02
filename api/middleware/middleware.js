d917e5df514b4af014de226393c71a51ba52408e
Katy Kane <katytheartist@gmail.com>
2024-05-01 21:01:26 -0500

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model.js');
+
 function logger(req, res, next) {
 const method = req.method
 const url = req.originalUrl
@@ -7,10 +9,23 @@ next()
 // DO YOUR MAGIC
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
 console.log('validateUserId middleware')
-next()
-  // DO YOUR MAGIC
+try {
+  const user = await User.getById(req.params.id)
+  if(!user){
+    res.status(404).json({
+      message: 'no such user,'
+    })
+  } else {
+    req.user = user
+    next()
+  }
+} catch (err){
+  res.status(500).json({
+    message: 'problem findin user'
+  })
+}
 }
 
 function validateUser(req, res, next) {
